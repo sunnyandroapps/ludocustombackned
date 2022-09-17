e15b70153fca588576433705d01438bd79f1bba8
Suraj <surajrawal.androapps@gmail.com>
2022-09-17 19:46:12 +0530

    Websocket

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-const express = require('express');
-const path = require('path');
-const WebSocket = require('ws'); // new
-const app = express();
-// express code 
-const socketServer = new WebSocket.Server({port: 1234});
-socketServer.on('connection', (socketClient) => {
-  console.log('connected');
-  console.log('client Set length: ', socketServer.clients.size);
-  socketClient.on('close', (socketClient) => {
-    console.log('closed');
-    console.log('Number of clients: ', socketServer.clients.size);
-  });
-});
+const SocketServer = require('websocket').server
+const http = require('http')
+
+const server = http.createServer((req, res) => {})
+
+server.listen(1234, ()=>{
+    console.log("Listening on port 1234...")
+})
+
+wsServer = new SocketServer({httpServer:server})
+
+const connections = []
+
+wsServer.on('request', (req) => {
+    const connection = req.accept()
+    console.log('new connection ===============')
+    connections.push(connection)
+
+    connection.on('message', (mes) => {
+        connections.forEach(element => {
+            if (element != connection)
+                element.sendUTF(mes.utf8Data)
+        })
+    })
+
+    connection.on('close', (resCode, des) => {
+        console.log('connection closed')
+        connections.splice(connections.indexOf(connection), 1)
+    })
+
+})
